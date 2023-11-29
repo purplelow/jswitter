5fd82330513d1e6bc62efe34e6debcb6c814cf8e
이준성(dri2104) <dri2104@gepplus.co.kr>
2023-11-29 14:29:48 +0900

    ...

---
+++ src/components/github-btn.tsx
@@ -0,0 +1,44 @@
+import { auth } from "../firebase";
+import { GithubAuthProvider, signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const Button = styled.span`
+  background-color: white;
+  font-weight: 600;
+  width: 100%;
+  color: black;
+  padding: 10px 50px;
+  border-radius: 50px;
+  border: 0;
+  display: flex;
+  gap: 5px;
+  align-items: center;
+  justify-content: center;
+  margin-top: 50px;
+  cursor: pointer;
+`;
+
+const Logo = styled.img`
+  height: 25px;
+`;
+
+export default function GithubButton() {
+  const navigate = useNavigate();
+  const onClick = async () => {
+    try {
+      const provider = new GithubAuthProvider();
+      await signInWithPopup(auth, provider);
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <Button onClick={onClick}>
+      <Logo src="src/assets/github-logo.svg" />
+      Continue with Github
+    </Button>
+  );
+}
--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,49 +1,18 @@
-import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { useState } from "react";
-import styled from "styled-components";
+import { Link, useNavigate } from "react-router-dom";
+import { FirebaseError } from "firebase/app";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+
 import { auth } from "../firebase";
-import { useNavigate } from "react-router-dom";
-
-const Wrapper = styled.div`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 420px;
-  padding: 50px 0px;
-`;
-
-const Title = styled.h1`
-  font-size: 42px;
-`;
-
-const Form = styled.form`
-  margin-top: 50px;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  width: 100%;
-`;
-
-const Input = styled.input`
-  padding: 10px 20px;
-  border-radius: 50px;
-  border: none;
-  width: 100%;
-  font-size: 16px;
-  font-weight: bold;
-  &[type="submit"] {
-    cursor: pointer;
-    &:hover {
-      opacity: 0.8;
-    }
-  }
-`;
-
-const Error = styled.span`
-  font-weight: 600;
-  color: tomato;
-`;
+import {
+  Error,
+  Form,
+  Input,
+  Switcher,
+  Title,
+  Wrapper,
+} from "../components/auth-components";
+import GithubButton from "../components/github-btn";
 
 export default function CreateAccount() {
   const navigate = useNavigate();
@@ -68,6 +37,7 @@ export default function CreateAccount() {
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setError("");
     if (isLoading || name === "" || email === "" || password === "") return;
     try {
       setLoading(true);
@@ -83,19 +53,21 @@ export default function CreateAccount() {
       });
       navigate("/");
     } catch (e) {
-      // setError
+      if (e instanceof FirebaseError) {
+        setError(e.message);
+      }
     } finally {
-      setLoading(true);
+      setLoading(false);
     }
     // create an account
     // set the name of the user
     // redirect to the home page
-    console.log(name, email, password);
+    // console.log(name, email, password);
   };
 
   return (
     <Wrapper>
-      <Title>Log into 𝕏</Title>
+      <Title>Join 𝕏</Title>
       <Form onSubmit={onSubmit}>
         <Input
           onChange={onChange}
@@ -127,6 +99,10 @@ export default function CreateAccount() {
         />
       </Form>
       {error !== "" ? <Error>{error}</Error> : null}
+      <Switcher>
+        Already have an account? <Link to={"/login"}>Log in &rarr;</Link>
+      </Switcher>
+      <GithubButton />
     </Wrapper>
   );
 }
